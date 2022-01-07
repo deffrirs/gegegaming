dd7d0d1c2e11fdc4cd3b6cef2ec6ac220eb59483
deffrirs <94781619+deffrirs@users.noreply.github.com>
2022-01-07 12:20:16 +0700

    Add files via upload
    P

--- shop/sewa.js
+++ shop/sewa.js
@@ -1,38 +1,40 @@
 const pc_sewa = (_minggu, __minggu, ___minggu, _bulan, _permanen, __permanen) => {
 return `「 *PRICE LIST* 」
 							
-⬣ PRICE SEWABOT
-
-
+「 *OPEN JASA SEWA BOT* 」
+							
+⬣ *HARGA SEWA BOT*
 
 ⬡ 1 MINGGU = Rp 10.000,00
-
 ⬡ 2 MINGGU = Rp 15.000,00
-
 ⬡ 3 MINGGU = Rp 20.000,00
-
 ⬡ 1 BULAN = Rp 30.000,00
-
 ⬡ PERMANEN = Rp 50.000,00
 
+「*FITUR MENARIK*」
+⬣ _5 set menu keren_✔️
+⬣ _Menu stiker_✔️
+⬣ _sticker meme_✔️
+⬣ _All menu download_✔️
+⬣ _Menu Game Beragam_✔️
+⬣ _Maker menu work_✔️
+⬣ _Menu Image Effect_✔️
+⬣ _*Dan Banyak Lagi Fitur Nya*_
 
+⬣ *PRICE JADIBOT*
+Fitur Jadi Bot Juga Tersedia Dan BOT Dijamin Run Otomatis 24jam no delay Tergantung Klw Nomor Ga terbanned.
 
+_Dibawah Berikut Harga Jadi BOT_
 
-⬣ PRICE JADIBOT
+⬡ JADI BOT 1 Bulan 10K
+⬡ Jadi BOT 2  Bulan 20k
+⬡ Jadi BOT PERMANEN 25K
 
-
-
-⬡ JADI BOT PERMANEN 25K
-
-
-
-
+⬣ _*TES BOT LINK DIBAWAH*_
+⬡ chat.whatsapp.com/CZiMSIfImb1LEcqFABOl4o
 
 ⬣ MINAT? PM
 ⬡ wa.me/6282132805286
-
-
-
 
 
 NOTE : 
